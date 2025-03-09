84af9c9bee38c55347cff7f69a0dffd96a0ee443
EricTLO <EricTLO@users.noreply.github.com>
2025-03-10 01:53:41 +0800

    Update AutoSyncProvider.tsx

--- app/AutoSyncProvider.tsx
+++ app/AutoSyncProvider.tsx
@@ -1,9 +1,17 @@
 // components/AutoSyncProvider.tsx
 "use client";
-import { useAutoSync } from './config/useAutoSync'; 
-import React from 'react';
+import { useAutoSync } from './config/useAutoSync';
+import React, { useEffect } from 'react';
 
 export function AutoSyncProvider({ children }: { children: React.ReactNode }) {
-    useAutoSync(); // 客户端组件中调用 Hook
-    return <>{children}</>;
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      useAutoSync(); // 每 5 分钟执行一次 useAutoSync
+    }, 5 * 60 * 1000); // 5 分钟 * 60 秒 * 1000 毫秒
+
+    // 组件卸载时清除 interval
+    return () => clearInterval(intervalId);
+  }, []); // 空依赖数组，确保只在组件挂载时设置 interval
+
+  return <>{children}</>;
 }
