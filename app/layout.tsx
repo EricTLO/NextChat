70d4c704e9011a2a4324b5216ed9bddea2d8c646
EricTLO <EricTLO@users.noreply.github.com>
2025-03-10 02:29:49 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 // app/layout.tsx
-import { AutoSyncProvider } from './AutoSyncProvider'; // 导入客户端组件
 
 export const metadata: Metadata = {
   title: "NextAIChat",
