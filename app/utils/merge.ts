baecfaf7185a248e786f1fd01264e5e7c0e5d0b0
EricTLO <EricTLO@users.noreply.github.com>
2025-03-26 23:11:55 +0800

    Update merge.ts

--- app/utils/merge.ts
+++ app/utils/merge.ts
@@ -1,13 +1,19 @@
 export function merge(target: any, source: any) {
   Object.keys(source).forEach(function (key) {
-    if (
-      source.hasOwnProperty(key) && // Check if the property is not inherited
-      source[key] &&
-      typeof source[key] === "object" || key === "__proto__" || key === "constructor"
-    ) {
-      merge((target[key] = target[key] || {}), source[key]);
-      return;
+    
+    const isObject = source[key] !== null && typeof source[key] === "object" && !Array.isArray(source[key]); // 明确排除数组
+    if (source.hasOwnProperty(key) && isObject) { // 只对真正的对象进行递归
+      if (
+        source.hasOwnProperty(key) && // Check if the property is not inherited
+        source[key] &&
+        typeof source[key] === "object" || key === "__proto__" || key === "constructor"
+      ) {
+        merge((target[key] = target[key] || {}), source[key]);
+        return;
+      }
     }
-    target[key] = source[key];
+    else if (source.hasOwnProperty(key)) { // 数组和其他类型会走这里
+     target[key] = source[key]; // 直接覆盖
+      }
   });
 } 
